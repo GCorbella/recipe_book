09c1863989b089a7611860d1e4756510e5713f33
Grifo55 <guillepcorbella@hotmail.com>
2022-06-14 13:33:39 +0200

    Objects needed imported, first functions created and first lines of code

---
+++ recipe_book.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from os import system
+working = True
+
+def welcome():
+
+def r_recipe():
+
+def n_recipe():
+
+def n_category():
+
+def x_recipe():
+
+def x_category():
+
+def shut_down():
+
+while working is True:
+    system(cls)
+    welcome()
